7af54c0b30a7754ff936b455c3294da98016f436
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2021-11-13 23:40:38 +0600

    last

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders)
         })
+        // get orders all product
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users)
+        })
         // post new product inside the products
         app.post('/products', async (req, res) => {
             const newPost = req.body;
@@ -97,6 +103,17 @@ async function run() {
             const updateDoc = { $set: { role: 'admin' } };
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
+        })
+        //  check admin states
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
         })
 
     }
